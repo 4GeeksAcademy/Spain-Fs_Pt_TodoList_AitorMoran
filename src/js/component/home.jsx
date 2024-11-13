39a98d6e8b42736def57d1bae3eaad46232dae9f
AitorMoran <aitorfm8@gmail.com>
2024-11-13 16:32:23 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,46 @@
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+//Variables
+
+const[inputValue, setInputValue] = useState ('');
+const [ToDoList, setToDoList] = useState ([]);
+
+const handleChange = e => setInputValue(prev => prev=e.target.value)
+
+const handleSubmit = e => {
+	e.preventDefault(); //evita el comportamiento habitual de los imput al actualizar
+	if (inputValue.trim() !== "") { //trim elimina espacios delante y detrás
+		setToDoList([...ToDoList, inputValue]);
+		setInputValue(''); //Limpia el input al escribir
+	}
+}
+
+const handleDelete = (index) => {
+	const newList = ToDoList.filter((task, i) => i !== index);
+	setToDoList(newList);
+}
+
+return (
+
+<div className="text-center">
+	<h1>My ToDoList</h1>
+	<h6>Pending Task: {ToDoList.length}</h6>
+		<form onSubmit={handleSubmit}>
+			<input type="text" value={inputValue} onChange={handleChange} placeholder="Add pending task" className="center_holder"/>
+		</form>
+		<ul>
+			{ToDoList.map((task, index) => (
+				<li key={index}>{task} <button type="onclick" onClick={() => handleDelete(index)}><span class="fa-solid fa-trash"></span></button> </li>
+			))}
+		</ul>
+	</div>
+);
 };
 
 export default Home;
+
+
